0ba72631479ac1263899d7436c391f432ce6c925
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2023-11-06 22:30:10 +0530

    adds myapply

---
+++ pollyfill/myApply.js
@@ -0,0 +1,13 @@
+Function.prototype.myApply = function (context = {}, args = []) {
+  if (typeof this !== "function") {
+    throw new Error(`${this} is not function`);
+  }
+  if (!Array.isArray(args)) {
+    throw new TypeError(`${args} should be Array`);
+  }
+
+  let x = this;
+  return function () {
+    x.apply(context, args);
+  };
+};
