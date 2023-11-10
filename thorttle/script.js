28b2cfbf4ad38b8c876664cbeb7c6653e7b1541a
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2023-11-10 23:15:14 +0530

    throttling vs debouncing

---
+++ thorttle/script.js
@@ -0,0 +1,42 @@
+const debouncing = function (callBack) {
+  var timer;
+
+  return (...args) => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      callBack(...args);
+    }, 1000);
+  };
+};
+
+const throttling = (callBack) => {
+  var timer;
+
+  return (...args) => {
+    if (timer) return null;
+    callBack(...args);
+    timer = setTimeout(() => {
+      timer = null;
+    }, 2000);
+  };
+};
+
+const debounce = debouncing((value) => {
+  console.log(value);
+});
+
+const input = document.getElementById("input");
+
+input.addEventListener("input", (e) => {
+  debounce(e.target.value);
+});
+
+const throttle = throttling((value) => {
+  console.log(value);
+});
+
+window.addEventListener("scroll", (e) => {
+  throttle(e);
+});
