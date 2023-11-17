af30e17eabedb3e1b64c8e858ec87700b0a651d3
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2023-11-17 23:12:02 +0530

    adds history support

--- mathsHistory/scripts/main.js
+++ mathsHistory/scripts/main.js
@@ -4,6 +4,7 @@
 
   const form = document.getElementById("equation-form");
   const btn = document.getElementById("search-btn");
+  const historyBtn = document.getElementById("history-btn");
 
   form.addEventListener("change", function (e) {
     const { value, name } = e.target;
@@ -16,9 +17,36 @@
   });
 
   btn.addEventListener("click", async function (e) {
+    clearSearchResult();
+    clearHistoryList();
     const result = await getEquationResults(params);
-    results.push(result);
+    results.unshift(result);
+    const searchResultEl = document.getElementById("search-result");
+
+    const article = createResultItem(result);
+    searchResultEl.append(article);
   });
+  historyBtn.addEventListener("click", function (e) {
+    clearForm();
+    clearSearchResult();
+    createHistoryList();
+  });
+
+  const clearForm = () => {
+    const input = document.getElementById("equation");
+    const category = document.getElementById("category");
+    input.value = "";
+    category.value = "";
+    params = {};
+  };
+  const clearSearchResult = () => {
+    const searchResultEl = document.getElementById("search-result");
+    searchResultEl.innerHTML = "";
+  };
+  const clearHistoryList = () => {
+    const historyList = document.getElementById("history-list");
+    historyList.innerHTML = "";
+  };
 
   const getEquationResults = async (params) => {
     try {
@@ -26,12 +54,49 @@
       if (!equation || !category) {
         return Promise.reject(new Error("Params are not correct"));
       }
-      const url = `https://newton.now.sh/api/v2/${category}/${equation}`;
+      const expression = encodeURIComponent(equation);
+      const url = `https://newton.now.sh/api/v2/${category}/${expression}`;
       const res = await fetch(url);
       const data = await res.json();
       return data;
     } catch (e) {
       console.error(e);
     }
+  };
+
+  const createHistoryList = () => {
+    const historyListEl = document.getElementById("history-list");
+    results.forEach((item) => {
+      const article = createResultItem(item, true);
+      historyListEl.append(article);
+    });
+  };
+
+  const createResultItem = (result, isHistory) => {
+    const article = document.createElement("article");
+    const operationEl = document.createElement("h3");
+    const expressionEl = document.createElement("p");
+    const resultEl = document.createElement("h4");
+
+    if (isHistory) {
+      var dltBtn = document.createElement("button");
+      dltBtn.innerHTML = "Delete";
+      //   var icon = document.createElement("i");
+
+      //   icon.classList.add("fa-solid fa-trash");
+      //   dltBtn.append(icon);
+    }
+
+    const { expression, operation, result: answer } = result;
+
+    operationEl.innerHTML = operation;
+    expressionEl.innerHTML = expression;
+    resultEl.innerHTML = answer;
+
+    article.append(operationEl, expressionEl, resultEl);
+    if (isHistory) {
+      article.append(dltBtn);
+    }
+    return article;
   };
 })();
