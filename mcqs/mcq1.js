f2d7f602b9071d8364e7489d53b8c081542b893a
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2023-11-20 23:14:16 +0530

    mcqs

---
+++ mcqs/mcq1.js
@@ -0,0 +1,45 @@
+//  What are the three phases of event propagation?
+// A: Target > Capturing > Bubbling
+// B: Bubbling > Target > Capturing
+// C: Target > Bubbling > Capturing
+// D: Capturing > Target > Bubbling
+
+// let number = 0;
+// console.log(number++);
+// console.log(++number);
+// console.log(number);
+
+// function getAge(...args) {
+//   console.log(args)
+//   console.log(typeof args);
+// }
+
+// getAge(21,23,22);
+
+//  How long is cool_secret accessible?
+
+// sessionStorage.setItem('cool_secret', 123);
+
+// A: Forever, the data doesn't get lost.
+// B: When the user closes the tab.
+// C: When the user closes the entire browser, not only the tab.
+// D: When the user shuts off their computer.
+
+// var num = 8;
+// let num = 10;
+
+// console.log(num);
+
+// const obj = { a: 'one', b: 'two', a: 'three' };
+// console.log(obj.a);
+
+// const a = {};
+// const b = { key: 'b' };
+// const c = { key: 'c' };
+// console.log(b.toString())
+// console.log(c.toString())
+
+// a[b] = 123;
+// a[c] = 456;
+
+// console.log(a[b]);
