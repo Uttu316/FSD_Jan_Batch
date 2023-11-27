80e181aa15ba1dad7da21294e1e8a34c6a71bc9f
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2023-11-27 23:52:54 +0530

    adds module type

--- jqueryProject/scripts/carousel.js
+++ jqueryProject/scripts/carousel.js
@@ -1,17 +1,4 @@
-// import { debouncing } from "./utils";
-
-const debouncing = function (callBack) {
-  var timer;
-
-  return (...args) => {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      callBack(...args);
-    }, 1000);
-  };
-};
+import { debouncing } from "./utils.js";
 
 $(() => {
   handleHeader();
@@ -21,24 +8,25 @@ $(() => {
     arrows: false,
   });
 
-  //   const showDebounce = debouncing(() => {
-  //     $(".header").show();
-  //   });
-  //   const hideDebounce = debouncing(() => {
-  //     $(".header").hide();
-  //   });
+  // use these function to see deboncing effect
+  const showDebounce = debouncing(() => {
+    $(".header").show();
+  });
+  const hideDebounce = debouncing(() => {
+    $(".header").hide();
+  });
 
   function handleHeader() {
     var lastScrollTop = 0;
 
     $(window).scroll(function () {
       var st = $(this).scrollTop();
-      if (st > lastScrollTop) {
+      if (st > lastScrollTop && st > 66) {
         // Scroll down
-        $(".header").hide();
+        $(".header").slideUp();
       } else {
         // Scroll up
-        $(".header").show();
+        $(".header").slideDown();
       }
       lastScrollTop = st;
     });
